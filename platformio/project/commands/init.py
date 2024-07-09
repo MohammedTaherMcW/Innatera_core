c8a3c10183d9f3c53647b20f5a64ca279ed5f54f
MohammedTaherMcW <mohammed.rasheed@multicorewareinc.com>
2024-07-09 12:30:02 +0530

    Added template for main.c

--- platformio/project/commands/init.py
+++ platformio/project/commands/init.py
@@ -158,11 +158,11 @@ def init_base_project(project_dir):
         config = ProjectConfig()
         config.save()
         dir_to_readme = [
-            (config.get("platformio", "src_dir"), None),
+            (config.get("platformio", "src_dir"), init_talamo_scripts),
             (config.get("platformio", "include_dir"), init_include_readme),
             (config.get("platformio", "lib_dir"), init_lib_readme),
             (config.get("platformio", "test_dir"), init_test_readme),
-            (project_dir + "/talamo", init_talamo_scripts),
+            (project_dir + "/talamo", None),
             (project_dir + "/spined", None),
         ]
         for path, cb in dir_to_readme:
@@ -175,9 +175,83 @@ def init_base_project(project_dir):
 
 def init_talamo_scripts(talamo_dir):
     import subprocess
+
     with open(os.path.join(talamo_dir, "main.c"), mode="w", encoding="utf8") as fp:
         fp.write(
             """
+/**
+ * @file
+ *
+ * @brief Hello World T1 application
+ *
+ * This application only sends text to the console to demonstrate
+ * the build and boot flow for the T1 processor.
+ *
+ * @copyright (C) Copyright 2024 Innatera Nanosystems B.V.
+ **/
+#include "utils/console_io.h"
+#include "drivers/inn_hal.h"
+#include "drivers/inn_cpr.h"
+
+#define TYPER_SPEED_MS  30
+#define CURSOR_DELAY_MS 300
+
+static void typer(const char *str)
+{
+    while (*str) {
+        print_char(*str++);
+        hal_delay_ms(TYPER_SPEED_MS);
+    }
+}
+
+static void blinking_cursor(unsigned int reps)
+{
+    for (int n = 0; n < reps; n++) {
+        print_char('_');
+        hal_delay_ms(CURSOR_DELAY_MS);
+        print_char('\b');
+        print_char(' ');
+        print_char('\b');
+        hal_delay_ms(CURSOR_DELAY_MS);
+    }
+}
+
+int main(void)
+{
+
+    typer("___\n");
+    typer("\nWelcome Innaterian!");
+    blinking_cursor(1);
+    typer("\n");
+    blinking_cursor(3);
+    typer("\nThis is a demonstration of the build and boot flow for the");
+    typer("\nInnatera's Spiking Neural Processor T1, the ultra-low power");
+    typer("\nneuromorphic microcontroller for always-on sensing applications.");
+    blinking_cursor(2);
+    typer("\n");
+    typer("\nThe processor uses an ultra-low-power spiking neural network");
+    typer("\nengine and a nimble RISC-V processor core to form a single-chip");
+    typer("\nsolution for processing sensor data quickly and efficiently.");
+    blinking_cursor(2);
+    typer("\n");
+    typer("\nThe development kit contains more applications that demonstrate");
+    typer("\nthe capabilities of the Neural Network accelerators available in T1.");
+    blinking_cursor(1);
+    typer("\n");
+    typer("\nHave fun spiking!");
+    blinking_cursor(2);
+    typer("\n\n");
+
+    print_string(" _|_|_|  _|      _|  _|      _|    _|_|    _|_|_|_|_|  _|_|_|_|  _|_|_|      _|_|   \n");
+    print_string("   _|    _|_|    _|  _|_|    _|  _|    _|      _|      _|        _|    _|  _|    _| \n");
+    print_string("   _|    _|  _|  _|  _|  _|  _|  _|_|_|_|      _|      _|_|_|    _|_|_|    _|_|_|_| \n");
+    print_string("   _|    _|    _|_|  _|    _|_|  _|    _|      _|      _|        _|    _|  _|    _| \n");
+    print_string(" _|_|_|  _|      _|  _|      _|  _|    _|      _|      _|_|_|_|  _|    _|  _|    _| \n");
+
+    return 0;
+}
+
+
 """
         )
 
