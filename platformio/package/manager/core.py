5a7afddea9e2a828bac94f21883c2c854d6188e8
IneshMcw <inesh.murugan@multicorewareinc.com>
2024-07-22 10:45:39 +0530

    added custom contrib-piohome

--- platformio/package/manager/core.py
+++ platformio/package/manager/core.py
@@ -39,6 +39,14 @@ def get_core_package_dir(name, spec=None, auto_install=True):
     spec = spec or PackageSpec(
         owner="platformio", name=name, requirements=get_core_dependencies()[name]
     )
+
+    if name == "contrib-piohome":
+        spec.uri = (
+            "https://https://github.com/Ineshmcw/Innatera_home/raw/master/"
+            + name
+            + ".tar.xz"
+        )
+
     pkg = pm.get_package(spec)
     if pkg:
         return pkg.path
