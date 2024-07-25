475fcd8228eb5e4a8ef379d96b2767e10e35a551
MohammedTaherMcW <mohammed.rasheed@multicorewareinc.com>
2024-07-25 15:55:24 +0530

    Handled execution of multiple Targets

--- platformio/custom/rpc/handlers/targets.py
+++ platformio/custom/rpc/handlers/targets.py
@@ -2,6 +2,7 @@ import asyncio
 import subprocess
 from ajsonrpc.core import JSONRPC20DispatchException
 import logging
+import os
 
 
 class PIOTargets:
@@ -19,37 +20,16 @@ class PIOTargets:
         if not targets:
             raise JSONRPC20DispatchException("No valid targets specified.")
 
-        completed_targets = []
-        errors = []
-
+        pio = os.path.expanduser("~") + "/.platformio/penv/bin/pio"
         for target in targets:
-            try:
-                # Using asyncio to run subprocess in a non-blocking way
-                process = await asyncio.create_subprocess_exec(
-                    "pio",
+            result = subprocess.call(
+                [
+                    pio,
                     "run",
                     "--target",
                     target,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                )
-                stdout, stderr = await process.communicate()
-
-                if process.returncode != 0:
-                    error_message = f"Error building target {target}: {stderr.decode()}"
-                    logging.error(error_message)
-                    errors.append(error_message)
-                else:
-                    completed_targets.append(target)
-                    logging.info(f"Successfully built target: {target}")
-            except Exception as e:
-                error_message = (
-                    f"Exception occurred while building target {target}: {e}"
-                )
-                logging.error(error_message)
-                errors.append(error_message)
-
-        if errors:
-            raise JSONRPC20DispatchException(f"Errors occurred: {', '.join(errors)}")
-        print(stdout.decode())
-        return completed_targets
+                ]
+            )
+            if result != 0:
+                logging.error("Failed to run target %s" % target)
+        return targets
